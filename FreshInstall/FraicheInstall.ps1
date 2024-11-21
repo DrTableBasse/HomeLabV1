4a575a922ac5ad9bd8db7c416ede3f5f2ebe680f
Samuel <nuez.samuel@gmail.com>
2024-11-21 19:09:43 +0100

    Create FreshInstall folder
    This is basically a script to install some soft on my pc when I finish to install Windows
    I'll do the same for Ubuntu and maybe for other OS.

---
+++ FreshInstall/FraicheInstall.ps1
@@ -0,0 +1,62 @@
+﻿if (-not ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Error "Ce script doit être exécuté avec des privilèges administratifs."
+    exit 1
+}
+function Install-Chocolatey {
+    if (-not (Get-Command choco -ErrorAction SilentlyContinue)) {
+        Write-Host "Chocolatey n'est pas installé. Installation en cours..."
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+        if ($LASTEXITCODE -ne 0) {
+            Write-Error "L'installation de Chocolatey a échoué."
+            exit 1
+        }
+        Write-Host "Chocolatey a été installé avec succès."
+    } else {
+        Write-Host "Chocolatey est déjà installé."
+    }
+}
+function Install-Packages {
+    param (
+        [string[]]$Packages
+    )
+    foreach ($package in $Packages) {
+        if (-not (choco list --local-only | Select-String $package)) {
+            Write-Host "Installation du package : $package..."
+            choco install $package -y
+            if ($LASTEXITCODE -ne 0) {
+                Write-Error "L'installation du package $package a échoué."
+            } else {
+                Write-Host "$package a été installé avec succès."
+            }
+        } else {
+            Write-Host "$package est déjà installé."
+        }
+    }
+}
+# Installation de Chocolatey
+Install-Chocolatey
+
+# Liste des packages à installer
+$packages = @(
+    "firefox",
+    "googlechrome",
+    "discord",
+    "git",
+    "xpipe",
+    "steam",
+    "github-desktop",
+    "vscode",
+    "obs-studio",
+    "teamviewer",
+    "anydesk",
+    "winscp",
+    #"docker-desktop", need some changes because dont install the right docker-desktop
+    "bitwarden",
+    "keepassxc",
+    "microsoft365",
+    "wireguard",
+    "wireshark"
+)
+Install-Packages -Packages $packages
---
+++ FreshInstall/ReadMe.md
@@ -0,0 +1,18 @@
+# Fresh Install
+
+## Presentation
+
+Fresh Install is a script designed for the fresh installation of Windows. It automates the process of downloading and installing software, and will soon be capable of configuring everything on the PC.
+
+So basically, it's a script that will save you a lot of time when you have to reinstall Windows.
+
+
+## How to use it
+
+Just run the script and follow the instructions. It's that simple.
+
+```powershell
+Set-ExecutionPolicy Unrestricted
+.\FreshInstall.ps1
+```
+warning: Don't forget to use it as Administrator.
